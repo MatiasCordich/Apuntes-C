26eb80c6aa4468c277eefff63dc712bbc128022b
MatiasCordich <m.scordich@gmail.com>
2023-01-07 22:06:11 -0300

    operadores logicos agregados

--- 04-operadores/main.c
+++ 04-operadores/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[])
 {
@@ -23,13 +24,13 @@ int main(int argc, char const *argv[])
 
     printf("%d\n", division);
 
-    // Primero creo una variable que guarde un numero de tipo decimal, pero como tanto x e y son de tipo entero, lo que tengo que hacer es CASTEAR la y, es decir, forzar su tipo de dato a tipo float. 
+    // Primero creo una variable que guarde un numero de tipo decimal, pero como tanto x e y son de tipo entero, lo que tengo que hacer es CASTEAR la y, es decir, forzar su tipo de dato a tipo float.
 
-    float casteo = x / (float) y;
+    float casteo = x / (float)y;
 
     printf("%f\n", casteo);
 
-    // Modulo 
+    // Modulo
 
     // El modulo me devuelve el resto de la division entre 5 y 2, el resto es 1
 
@@ -51,12 +52,56 @@ int main(int argc, char const *argv[])
 
     int num = 10, sum, res, mul, div;
 
-    //num+=1; // num = num + 1
-    //num-=2; // num = num - 1
-    //num*=4; // num = num * 4
-    num/=2; // num = num / 2
+    // num+=1; // num = num + 1
+    // num-=2; // num = num - 1
+    // num*=4; // num = num * 4
+    num /= 2; // num = num / 2
 
     printf("%d\n", num);
+
+    /* Operadores logicos */
+
+    /*
+        Existen 3 tipos de operadores logicos, los cuales son
+
+        && = AND
+        || = OR
+        ! = NOT
+
+        Estos operadores nos van a servir para evaluar dos o mas condiciones. El operador ! se utiliza para cambiar el valor opuesto de la variable.
+
+    */
+
+    // Operador AND &&
+
+    // Este operador sirve para evaluar dos o mas condiciones, dichas condiciones SE TIENEN QUE CUMPLIR TODAS SI O SI
+
+    float temp = -18;
+
+    if (temp <= 0 && temp <= 10) // La condicion es que la temperatura tiene que ser mayor o igual a 0 Y (AND) menor o igual a 10, si el valor de la variable cumple con ambas condiciones se ejecuta el codigo
+    {
+        printf("Hace frio\n");
+    }
+    else if (temp >= 11 && temp <= 20) // La condicion es que la temperatura tiene que ser mayor o igual a 11 Y (AND) menor o igual a 20, si el valor de la variable cumple con ambas condiciones se ejecuta el codigo
+    {
+        printf("Esta templado\n");
+    }
+    else
+    {
+        printf("Hace calor\n");
+    }
+
+    // Operador OR ||
+
+    // Este operador evalua dos o mas condiciones y con que SOLAMENTE UNA CONDICION sea verdadera es suficiente.
+
+    if (temp < 0 || temp == 0 || temp <= 10) // La condiciones son que si la temperatura es menor a cero O (OR) la temperatura es cero O si la temperatura es menor o igual a 10 entonces ejecutame el siguiente codigo. Solo se tiene que cumplir una de las condiciones
+    {
+        printf("Hace frio\n");
+    }
+    else {
+        printf("Esta templado\n");
+    }
 
     return 0;
 }
