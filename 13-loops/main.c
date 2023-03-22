a7f4f2bb130d081290541174fb2311a8dde02dcc
MatiasCordich <m.scordich@gmail.com>
2023-03-21 23:13:43 -0300

    added loops subject

---
+++ 13-loops/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // Bucle For
+
+    // El Bucle For consta de la siguiente estructura
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%i\n", i);
+    }
+
+    // el for consta de 3 declaraciones for(1,2,3)
+
+    // 1 - La variable con la que inicializamos el bucle, que en este caso es de tipo int. i
+    // 2 - La condicion que le damos a ese loop, en este casi que me repitas la ejecucion mientras que i sea MENOR a 10
+    // 3- Aumentarle a la variable i una unidad mas
+
+    return 0;
+}
