0fcd90c4223d4c7d120175fdc051be2399c12666
MatiasCordich <m.scordich@gmail.com>
2023-04-04 11:13:35 -0300

    ejercicio de productos modificado

--- Tareas/Productos/main.c
+++ Tareas/Productos/main.c
@@ -5,29 +5,29 @@ int main(int argc, char const *argv[])
     // INGRESAR EL PRECIO, STOCK Y COSTO Y CON ESOS VALORES DETERMINAR LA GANANCIA UNITARIA, COSTO TOTAL y GANANCIA TOTAL
 
     // Determino las variables 
-    int precio;
+    int precio_unitario;
     int stock;
-    int costo;
-    int gananciaUnitaria;
-    int costoTotal;
-    int gananciaTotal;
+    int costo_unitario;
+    int ganacia_unitaria;
+    int costo_total;
+    int ganancia_total;
 
     // Ingreso por teclado los valores de cada variable que se van a ir almacenando en cada una
     printf("Ingrese el precio: ");
-    scanf("%i", &precio);
+    scanf("%i", &precio_unitario);
     printf("Ingrese costo: ");
-    scanf("%i", &costo);
+    scanf("%i", &costo_unitario);
     printf("Ingrese el stock: ");
     scanf("%i", &stock);
 
     // Una vez ingresado los valores de las variables precio, costo y stock determinar lo que se pide mediante operaciones artimeticas
-    gananciaUnitaria = precio - costo;
-    costoTotal = stock * costo;
-    gananciaTotal = gananciaUnitaria * stock;
+    ganacia_unitaria = precio_unitario - costo_unitario;
+    costo_total = stock * costo_unitario;
+    ganancia_total = ganacia_unitaria * stock;
 
     // Muestro por pantalla los resultados
-    printf("La ganacia unitaria es de: %i\n", gananciaUnitaria);
-    printf("El costo total es de: %i\n", costoTotal);
-    printf("La ganacia total es de: %i\n", gananciaTotal);
+    printf("La ganacia unitaria es de: %i\n", ganacia_unitaria);
+    printf("El costo total es de: %i\n", costo_total);
+    printf("La ganacia total es de: %i\n", ganancia_total);
     return 0;
 }
