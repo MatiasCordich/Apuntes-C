4b958801c149d53db745e8f5a50855b245c03882
MatiasCordich <m.scordich@gmail.com>
2023-04-09 21:45:03 -0300

    cambios guardados

--- Tareas/Ejercicios-guía/EJ-8/main.c
+++ Tareas/Ejercicios-guía/EJ-8/main.c
@@ -7,14 +7,14 @@ int main(int argc, char const *argv[])
     int celsius;
     float fahrenheit;
     
-    // Ingreso un valor numerico que sera guardado en la variable celsius
+    // Ingreso un valor numerico que sera guardado en la variable celsius.
     printf("Ingrese los grados celsius: ");
     scanf("%i", &celsius);
 
-    // Con el valor de la variable celsius calculo los grados fahrenheit
+    // Con el valor de la variable celsius calculo los grados fahrenheit.
     fahrenheit = (celsius * 9/5) + 32;
 
-    // Imprimo por pantalla la conversion de celcius a fahrenheit
+    // Imprimo por pantalla la conversion de celcius a fahrenheit.
     printf("%i grados celsius son %f grados fahrenheit", celsius, fahrenheit);
     
 
