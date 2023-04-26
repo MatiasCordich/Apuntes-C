1a91c0fe33ca929ee62760b8932dbb5b055342fe
MatiasCordich <m.scordich@gmail.com>
2023-04-25 21:34:21 -0300

    ej 35 resuelto

--- Tareas/Ejercicios-guía/EJ-35/main.c
+++ Tareas/Ejercicios-guía/EJ-35/main.c
@@ -5,8 +5,8 @@ int main(int argc, char const *argv[])
     // DE LOS ALUMNOS DE LA ESCUELA SE REGISTRA SU APELLIDO Y SU NOMBRE Y SU ALTURA. DISEÑAR EN PSEUDOCODIGO UN ALGORITMO QUE INDIQUE EL NOMBRE DEL ALUMNO MÁS ALTO Y SU ALTURA (SOLO UNO ES EL MÁS ALTO).
 
     // Declar mis variables
-    char nombre_alumno[50];
-    char nombre_alumno_mas_alto[50];
+    char nombre_alumno;
+    char nombre_alumno_mas_alto;
     float altura_alumno;
     float alumno_mas_alto = 0;
     char respuesta;
@@ -17,15 +17,15 @@ int main(int argc, char const *argv[])
 
         // Ingreso el nombre del alumno
         printf("Ingrese el nombre del alumno: ");
-        scanf("%s", nombre_alumno);
+        scanf(" %c", &nombre_alumno);
 
         // Ingreso la altura del alumno
-        printf("Ingrese la altura del aluno: ");
+        printf("Ingrese la altura del alumno: ");
         scanf(" %f", &altura_alumno);
 
         // Calculo la altura maxima y guardo la altura y el nombre del alumno mas alto
         if(altura_alumno > alumno_mas_alto) {
-            nombre_alumno_mas_alto[50] = nombre_alumno; // El problema esta aca que no puedo asignarle un string 
+            nombre_alumno_mas_alto = nombre_alumno; // El problema esta aca que no puedo asignarle un string 
             alumno_mas_alto = altura_alumno;
         }
 
@@ -38,7 +38,7 @@ int main(int argc, char const *argv[])
     
 
     // Escribo la respuesta
-    printf("El alumno mas alto es %s con %.2f metros", nombre_alumno_mas_alto, alumno_mas_alto);
+    printf("El alumno mas alto es %c con %.2f metros", nombre_alumno_mas_alto, alumno_mas_alto);
 
 
     return 0;
