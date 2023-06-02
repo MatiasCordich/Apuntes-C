3968bbb22caff4601dcb3548defa3ae67f955172
MatiasCordich <m.scordich@gmail.com>
2023-06-01 23:11:58 -0300

    agenda creada

--- Tareas/Agenda/main.c
+++ Tareas/Agenda/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+typedef struct
+{
+    char nombre;
+    char apellido;
+    int dni;
+    int telefono;
+    int anio_nacimiento;
+}s_persona;
+
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
     return 0;
 }
