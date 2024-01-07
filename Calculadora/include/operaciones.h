28b41e69f7b3c0030f965fecea31909badb45ddd
MatiasCordich <m.scordich@gmail.com>
2024-01-06 21:05:02 -0300

    CALCULADORA 2.0 version

---
+++ Calculadora/include/operaciones.h
@@ -0,0 +1,10 @@
+#ifndef OPERACIONES_H
+#define OPERACIONES_H
+
+// Declaramos las funciones para operaciones artimeticas
+float sumar(float num_1, float num_2);
+float restar(float num_1, float num_2);
+float multiplicar(float num_1, float num_2);
+float dividir(float num_1, float num_2);
+
+#endif
